ab655f38e919d28559ae6d92dfa2e5a6f2c00e5d
Habin007 <habin1647@gmail.com>
2025-12-17 00:35:28 +0700

    Coding_w02-01

---
+++ image_coding_week-02/hello_world.py
@@ -0,0 +1,13 @@
+# hello_world.py
+# Code for Coding_w02-01: Hello, World practice.
+
+# Define the message to be displayed.
+message = "Hello, World! From Image Processing Class"
+
+# Display the message to the console.
+print(message)
+
+# additional information: Display student name and ID (for code documentation).
+print("--------------------------------------------------")
+print("Prepared by: [ภูรพัฒน์ ธนประสิทธิ์พัฒนา]")
+print("Student ID: [6612022611091]")
